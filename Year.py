142124e8bd7d78e8cf9c8ea5e4a25014fda0b58b
songx2-xy <songx2@oregonstate.edu>
2021-03-07 23:58:21 -0800

    Implemented x method,{In this file,I added the method that how to calculate the leap year, if a number can be divided by 4, 100, 400, it will be a leap year, and the out put should be leap year, if the number cannot be divided by those 3 numbers, it's not a leap year, and will return to not a leap year.}

--- Year.py
+++ Year.py
@@ -1,2 +1,7 @@
 def Year(y):
-    return "Not Leap Year"
+    if y%4==0 and y%100==0 and y%400==0:
+        return "Leap Year"
+    else:
+        return "Not Leap Year"
+print(Year(2001))
+print(Year(2000))
