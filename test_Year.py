3c4fc1d74178092115cf4bb57a4eb5b9200a72a5
songx2-xy <songx2@oregonstate.edu>
2021-03-08 00:01:35 -0800

    Implemented x method,{in this test file, I added a new function, the new function is to test if the input is a leap year, such as 2000, 1600, the output should be Leap Year. And the test is pass.}

--- test_Year.py
+++ test_Year.py
@@ -3,5 +3,10 @@ from Year import*
 class TestCase(unittest.TestCase):
     def test_Year1(self):
         self.assertEqual(Year(2002), "Not Leap Year")
+        self.assertEqual(Year(2021), "Not Leap Year")
+        self.assertEqual(Year(1999), "Not Leap Year")
+    def test_Year2(self):
+        self.assertEqual(Year(2000), "Leap Year")
+        self.assertEqual(Year(1600), "Leap Year")
 if __name__=='__main__':
     unittest.main()
