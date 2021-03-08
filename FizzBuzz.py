47ed1fafd8053564940be5c344987f44a56f930f
songx2-xy <songx2@oregonstate.edu>
2021-03-07 22:36:06 -0800

    Implemented x method,{in the FizzBuzz.py, I added if the number can be both devided by 3 and 5, it will return FizzBuzz at the top of my function. So if the input is 15, it should print FizzBuzz.}

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,9 @@
 def FizzBuzz(n):
-    if n%3==0:
-        return "Fizz"
+    if n%5==0 and n%3==0:
+        return "FizzBuzz"
     elif n%5==0:
         return "Buzz"
+    elif n%3==0:
+        return "Fizz"
     else:
         return n
