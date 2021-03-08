d9130f10acdf3165c215ce8d0e8351506c0fa7b1
songx2-xy <songx2@oregonstate.edu>
2021-03-07 23:48:09 -0800

    Implementedxmethod,{In this test file, I keep the initial test,the initial test is input 2002, and the output should be no a leap year. Because I write some code in Year.py, so this test is pass.}

--- test_Year.py
+++ test_Year.py
@@ -2,6 +2,6 @@ import unittest
 from Year import*
 class TestCase(unittest.TestCase):
     def test_Year1(self):
-        self.assertEqual(Year(2002), "No")
+        self.assertEqual(Year(2002), "Not Leap Year")
 if __name__=='__main__':
     unittest.main()
