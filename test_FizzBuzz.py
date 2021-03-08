1eeeba338863457b761195b968591d62b64b3306
songx2-xy <songx2@oregonstate.edu>
2021-03-07 22:27:07 -0800

    Implemented x method,{in the test file, I added a new test function to test if the number is 3, 6, 9, the output should be Fizz, and it passed.}

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -5,6 +5,10 @@ class TestCase(unittest.TestCase):
         self.assertEqual(FizzBuzz(1), 1)
         self.assertEqual(FizzBuzz(2), 2)
         self.assertEqual(FizzBuzz(7), 7)
+    def test_FizzBuzz2(self):
+        self.assertEqual(FizzBuzz(3), "Fizz")
+        self.assertEqual(FizzBuzz(6), "Fizz")
+        self.assertEqual(FizzBuzz(9), "Fizz")
 if __name__=='__main__':
     unittest.main()
     
