fa8850163742003299107a47cd65eb6011bc1a55
songx2-xy <songx2@oregonstate.edu>
2021-03-07 22:22:10 -0800

    Implemented x method, {in the test file, I add two results one is if n is 2, it should return to 2, and if the n is 7, the result should be 7}

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -3,6 +3,8 @@ from FizzBuzz import*
 class TestCase(unittest.TestCase):
     def test_FizzBuzz1(self):
         self.assertEqual(FizzBuzz(1), 1)
+        self.assertEqual(FizzBuzz(2), 2)
+        self.assertEqual(FizzBuzz(7), 7)
 if __name__=='__main__':
     unittest.main()
     
